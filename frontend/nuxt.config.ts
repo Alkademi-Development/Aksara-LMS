3c25982ead785c476f9a9c6a98d988cf20433eb8
Nama Anda <email@example.com>
2025-04-30 13:55:24 +0700

    (+) remove NUXT indicator for environment variable and disabled devtools when it's on production

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV != 'production' },
   runtimeConfig: {
-    baseURL: process.env.NUXT_BASE_URL,
-    servicesApi: process.env.NUXT_SERVICES_API,
-    vueAppMode: process.env.NUXT_VUE_APP_MODE,
+    baseURL: process.env.BASE_URL,
+    servicesApi: process.env.SERVICES_API,
+    vueAppMode: process.env.VUE_APP_MODE,
     public: {
-      baseURL: process.env.NUXT_BASE_URL,
-      servicesApi: process.env.NUXT_SERVICES_API,
-      vueAppMode: process.env.NUXT_VUE_APP_MODE,
+      baseURL: process.env.BASE_URL,
+      servicesApi: process.env.SERVICES_API,
+      vueAppMode: process.env.VUE_APP_MODE,
     },
   },
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
